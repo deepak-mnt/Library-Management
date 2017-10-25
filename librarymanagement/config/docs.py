5476a1c327120a89dff051a7f0c853a701a7e524
deepak-mnt <deepak@mntechnique.com>
2017-10-25 16:16:18 +0530

    Added documentation WIP

--- librarymanagement/config/docs.py
+++ librarymanagement/config/docs.py
@@ -2,10 +2,10 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/librarymanagement"
-# docs_base_url = "https://[org_name].github.io/librarymanagement"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/deepak-mnt/librarymanagement"
+docs_base_url = "https://deepak-mnt.github.io/librarymanagement"
+headline = "Managing and creating records for libraries"
+sub_heading = "App for managing Articles, Members, Memberships and Transactions for Libraries"
 
 def get_context(context):
 	context.brand_html = "Library Management"
